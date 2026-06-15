f9c27ea1d0ac0e9f23c1ee41871ae0f19c47d56c
mathe-man <mathieumousset6@gmail.com>
2026-06-15 19:09:43 +0200

    Create a basci screen controller (ILI9341 screen)

---
+++ include/ILI9341Controller.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include <ScreenController.h>
+
+// Platformio lib_deps =
+// lovyan03/LovyanGFX
+#include <LovyanGFX.hpp>
+
+struct Pinout {
+    uint16_t pin_cs;
+    uint16_t pin_reset;
+    uint16_t pin_dc;
+    uint16_t pin_mosi;
+    uint16_t pin_SCK;
+    uint16_t pin_miso;
+};
+
+class ILI9341Controller : public ScreenController, public lgfx::LGFX_Device {
+    lgfx::Panel_ILI9341 _panel_instance;
+    lgfx::Bus_SPI       _bus_instance;
+
+
+public:
+    ILI9341Controller(Pinout pins, uint16_t width = 240, uint16_t height = 320, uint32_t freq_write = 40000000, uint32_t freq_read = 16000000);
+
+    virtual ~ILI9341Controller() =  default;
+
+    void begin() { init(); }
+
+    // TODO implement the ScreenController methods
+};
---
+++ src/ILI9341Controller.cpp
@@ -0,0 +1,35 @@
+﻿#include <ILI9341Controller.h>
+
+ILI9341Controller::ILI9341Controller(Pinout pins, uint16_t width, uint16_t height, uint32_t freq_write, uint32_t freq_read) {
+
+    // SPI bus configuration
+    {
+        auto cfg = _bus_instance.config();
+        cfg.spi_host = SPI2_HOST;   // FSPI bus of the ESP32-S3
+        cfg.spi_mode = 0;
+        cfg.freq_write = freq_write;
+        cfg.freq_read  = freq_read;
+        cfg.pin_sclk = pins.pin_SCK;            // PINS
+        cfg.pin_mosi = pins.pin_mosi;
+        cfg.pin_miso = pins.pin_miso;
+        cfg.pin_dc   = pins.pin_dc;
+        _bus_instance.config(cfg);
+        _panel_instance.setBus(&_bus_instance);
+    }
+
+    // Screen panel configuration
+    {
+        auto cfg = _panel_instance.config();
+        cfg.pin_cs           = pins.pin_cs;
+        cfg.pin_rst          = pins.pin_reset;
+        cfg.pin_busy         = -1;
+        cfg.panel_width      = width;
+        cfg.panel_height     = height;
+        cfg.offset_x         = 0;
+        cfg.offset_y         = 0;
+        cfg.rgb_order        = false;
+        _panel_instance.config(cfg);
+    }
+
+    setPanel(&_panel_instance);
+}
